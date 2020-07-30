1daa688248dcd3c5fd19ff192a736f1a6d25f6ee
AMH <waddley8@gmail.com>
2020-07-29 22:01:32 -0400

    vectors

--- sketch.js
+++ sketch.js
@@ -2,13 +2,9 @@ let bubbles = [];
 let letters = ['2','3','4','5','A','C','J','Z','R','W'];
 let colors = ['#ee352e','#ee352e','#00933c','#00933c','#2850ad','#2850ad','#996633','#996633','#fccc0a','#fccc0a']
 let mouse = {
-  x: -1,
-  y: -1,
   size: 20
 }
 let testBubble = {
-  x: -1,
-  y: -1,
   size: 1
 }
 let maxBubbles = 8;
@@ -16,6 +12,8 @@ let maxBubbles = 8;
 function setup() {
   createCanvas(min(windowWidth,windowHeight), min(windowWidth,windowHeight));
   noStroke();
+  mouse.pos = createVector(-1, -1);
+  testBubble.pos = createVector(-1, -1);
   for (var i = 0; i < letters.length; i++) {
     bubbles.push(new Bubble(random(width), random(height), letters[i], color(colors[i]),1));
   }
@@ -23,8 +21,6 @@ function setup() {
 
 function draw() {
   background(220);
-  mouse.x = mouseX;
-  mouse.y = mouseY;
   //grow any bubble that isn't touching anything
   for (let i = bubbles.length - 1; i >= 0; i--) {
     if (!bubbles[i].touchingAny()) {
@@ -42,12 +38,10 @@ function touchMoved(){
   return false;
 }
 function popBubbles() {
-  mouse.x = mouseX;
-  mouse.y = mouseY;
+  mouse.pos.set(mouseX, mouseY);
   for (let i = bubbles.length - 1; i >= 0; i--) {
     if (bubbles[i].touching(mouse)) {
-      testBubble.x = random(width);
-      testBubble.y = random(height);
+      testBubble.pos.set(random(width), random(height));
       testBubble.size = 1;
       let touching = 0;
       for (let j = bubbles.length - 1; j >= 0; j--) {
@@ -56,8 +50,7 @@ function popBubbles() {
         }
       }
       if (touching == 0) {
-        bubbles[i].x = testBubble.x;
-        bubbles[i].y = testBubble.y;
+        bubbles[i].pos.set(testBubble.pos);
         bubbles[i].size = 1;
       }
     }
